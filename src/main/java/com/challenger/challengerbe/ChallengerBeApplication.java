77ea8359abbfedc4db02a1bac66c40077adca39f
rhkdgur <rhkdgur195@gmail.com>
2024-09-07 05:44:38 +0900

    환경설정

---
+++ src/main/java/com/challenger/challengerbe/ChallengerBeApplication.java
@@ -0,0 +1,13 @@
+package com.challenger.challengerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengerBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengerBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenger/challengerbe/ChallengerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenger.challengerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengerBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
